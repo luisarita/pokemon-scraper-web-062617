5a6de11b18da98cc902811a96f98de2fe4703779
Luis Arita <github@luisarita.com>
2017-07-06 15:33:26 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,7 +1,10 @@
 class Pokemon
     attr_reader :id, :name, :type, :db
+    attr_accessor :hp
+
     def initialize(id: nil, name: nil, type: nil, db: nil)
         @id, @name, @type, @db = id, name, type, db
+        @hp = 60
     end
     def self.save(name, type, db)
         #pokemon = Pokemon.new(row[0], row[1], @db)
@@ -12,7 +15,22 @@ class Pokemon
         sql_statement = "SELECT id, name, type FROM pokemon WHERE id=#{id}"
         rows = db.execute(sql_statement)
         row = rows[0]
-        Pokemon.new(id: row[0], name: row[1], type: row[2], db: db)
+        new_pokemon = Pokemon.new(id: row[0], name: row[1], type: row[2], db: db)
+
+        #We're doing in a separate query since the tests are made to run before the alter
+        begin
+            sql_statement = "SELECT hp FROM pokemon WHERE id=#{id}"
+            rows = db.execute(sql_statement)
+            new_pokemon.hp = rows[0][0]
+        rescue SQLite3::SQLException
+        end
+        new_pokemon
+    end
+
+    def alter_hp(hp, db)
+        @hp = hp
+        sql_statement = "UPDATE pokemon SET hp=#{hp} WHERE id=#{@id}"
+        db.execute(sql_statement)
     end
     
 end
